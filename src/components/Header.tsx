e1eef73474532f6a4a41f72885d64a97f2123fc3
Impulse <impulses4mini@gmail.com>
2024-05-11 15:09:07 +0300

    add imports to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,14 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from './ui/button';
+import { useState } from 'react';
+
 const Header = () => {
-  return <div>Header</div>;
+  const handleLogout = () => {};
+
+  return (
+    <header>
+      <div className='align-element flex justify-center sm:justify-end py-2'></div>
+    </header>
+  );
 };
 export default Header;
