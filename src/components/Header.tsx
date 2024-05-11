4f57d82941c3a4a3bd51c88afe3328dab9bc9588
Impulse <impulses4mini@gmail.com>
2024-05-11 15:42:38 +0300

    add Sign in and Register Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { Button } from './ui/button';
-import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 const Header = () => {
   const [user, setUser] = useState<{ username: string } | null>({
-    username: 'demo user',
+    username: "demo user",
   });
   // const [user, setUser] = useState<{ username: string } | null>(null);
   const handleLogout = () => {
@@ -13,20 +13,29 @@ const Header = () => {
 
   return (
     <header>
-      <div className='align-element flex justify-center sm:justify-end py-2 mr-6'>
+      <div className="align-element mr-6 flex justify-center py-2 sm:justify-end">
         {user ? (
-          <Button
-            onClick={() => {
-              setUser(null);
-            }}
-            size='lg'
-          >
-            Logout
-          </Button>
+          <div className="flex items-center gap-x-4 sm:gap-x-8">
+            <p>Hello, {user.username}</p>
+            <Button
+              variant="link"
+              onClick={() => {
+                setUser(null);
+              }}
+              size="sm"
+            >
+              Logout
+            </Button>
+          </div>
         ) : (
-          <div className='flex gap-x-2  sm:gap-x-8 items-center'>
-            <Link to='/register'>register</Link>
-            <Link to='/login'>login</Link>
+          <div className="flex items-center  gap-x-4 sm:gap-x-8">
+            <Button asChild variant="link" size="sm">
+              <Link to="/login">Sign in / Guest</Link>
+            </Button>
+
+            <Button asChild variant="link" size="sm">
+              <Link to="/register">Register</Link>
+            </Button>
           </div>
         )}
       </div>
