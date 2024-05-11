26c75b6324c5f3644aecf76198532c8d8c1dc93a
Impulse <impulses4mini@gmail.com>
2024-05-11 20:57:08 +0300

    add menu

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -1,4 +1,25 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AlignLeft } from "lucide-react";
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <AlignLeft />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
 export default LinksDropdown;
