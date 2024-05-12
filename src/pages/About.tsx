7863135889ec6c44b34c16c34724e86a083a7624
Impulse <impulses4mini@gmail.com>
2024-05-12 19:08:07 +0300

    add About Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,20 @@
 const About = () => {
   return (
-    <div className="flex flex-col items-center">
-      {/* <div>
-        <h1 className="inline-block text-6xl">We love</h1>
-        <span>comfy</span>
-      </div> */}
-    </div>
+    <section>
+      <h1 className="flex flex-wrap items-center justify-center gap-2 text-4xl font-bold leading-none tracking-tight sm:gap-x-6 sm:text-6xl ">
+        We love
+        <span className="rounded-lg bg-primary px-4 py-2  tracking-widest text-white">
+          comfy
+        </span>
+      </h1>
+
+      <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 tracking-wide">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam
+        odit, officiis eos mollitia alias, doloremque, aspernatur ratione
+        asperiores voluptas labore minus dolores reprehenderit corporis quos.
+        Assumenda molestias harum dignissimos?
+      </p>
+    </section>
   );
 };
 
