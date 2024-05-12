2c046db0e571b1fb9fba2c91b2fc5c997ff28cfd
Impulse <impulses4mini@gmail.com>
2024-05-12 19:43:39 +0300

    fetch featured products

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,17 @@
 import { Hero, FeaturedProducts } from "@/components/index";
 import { customFetch } from "@/utils/index";
+import { useLoaderData } from "react-router-dom";
+
+const url = "/products?featured=true";
 
 export async function loader() {
-  const data = await customFetch("/products");
+  const data = await customFetch(url);
   return { products: data.data };
 }
 
 const Landing = () => {
+  const { products } = useLoaderData();
+  console.log(products);
   return (
     <>
       <Hero />
