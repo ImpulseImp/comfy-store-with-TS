fbf827dbb37b1cb77b5675a79fe88a3c66a20e58
Impulse <impulses4mini@gmail.com>
2024-05-21 22:05:33 +0300

    add query params

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,28 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData, Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "./ui/button";
 
-const Filters = () => {
-  return <Form></Form>;
-};
+function Filters() {
+  return (
+    <Form className="grid items-center gap-x-4 gap-y-4 rounded-md border px-8 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="mb-2">
+        <Label htmlFor="search">Search Product</Label>
+        <Input id="search" name="search" type="text" defaultValue="" />
+      </div>
+      <Button type="submit" size="sm" className="mb-2 self-end">
+        search
+      </Button>
+      <Button
+        type="button"
+        asChild
+        size="sm"
+        variant="outline"
+        className="mb-2 self-end"
+      >
+        <Link to="/products">reset</Link>
+      </Button>
+    </Form>
+  );
+}
 export default Filters;
